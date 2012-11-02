f56f983f6d27cf5f3adb258f7ba48e97e1f037ef
Conor McLaughlin <conoremclaughlin@gmail.com>
2012-11-02 13:40:25 -0700

    Making CRUD api path more flexible

--- cc-core/models/Base.server.bones.js
+++ cc-core/models/Base.server.bones.js
@@ -8,10 +8,29 @@ model.prototype.dbSchema = model.dbSchema = {};
 model.augment({
     initialize: function(parent) {
         parent.call(this);
-        model.permissions = ;
         if (!Bones.plugin.app || !Bones.plugin.app.db) { console.error('ERROR: no backend available.'); }
         // bones is awesome so it exposes a model's title for our use :]
         this.db = new Bones.plugin.app.db(this.title, new mongoose.Schema(this.dbSchema));
+    },
+    validate: function(parent, req, res, next) {
+        var errors = parent ? parent.call(this) : true;
+        /*
+         * Example use of express-validator in model.validate() to both validate and sanitize.
+         *
+        req.assert('postparam', 'Invalid postparam').notEmpty().isInt();
+        req.assert('getparam', 'Invalid getparam').isInt();
+        req.assert('urlparam', 'Invalid urlparam').isAlpha();
+
+        req.sanitize('postparam').toBoolean();
+
+        errors = errors && req.validationErrors();
+        if (errors) {
+          res.send('There have been validation errors: ' + util.inspect(errors), 500);
+          return;
+        }
+        // else
+        return next();
+        */
     }
 });
 
@@ -22,3 +41,5 @@ model.prototype.permissions = {
     'update': {},
     'delete': {}
 };
+
+model.prototype
