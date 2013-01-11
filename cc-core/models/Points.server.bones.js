77d7cb6b9229f0d253f8fb9fd76545b77482d978
Conor McLaughlin <conoremclaughlin@gmail.com>
2013-01-10 16:16:37 -0800

    Returned to original developmentseed/bones for the server fix.

--- cc-core/models/Points.server.bones.js
+++ cc-core/models/Points.server.bones.js
@@ -3,22 +3,4 @@ model = models.Points;
 Bones.plugin.backends.MonEnumerable.mixinQueries(model);
 
 // example use:
-//Bones.plugin.backends.MonEnumerable.mixinPrototypeQueries(model, model.prototype.getConnection);
-
-console.log('Collection Points: ', util.inspect(model));
-
-/*
-model.getLatest = function(skip, limit, callback) {
-    callback = callback || _.last(arguments);
-    if (!callback || !_.isFunction(callback)) return false;
-    skip = skip || 0;
-    limit = limit || 10;
-    // XXX: fix this query. different from find for some reason.
-    Bones.plugin.mongooseModels[model.prototype.model.title]
-        .find()
-        .sort('-created')
-        .skip(skip)
-        .limit(limit)
-        .exec(callback);
-    return true;
-};*/
+// Bones.plugin.backends.MonEnumerable.mixinPrototypeQueries(model, model.prototype.getConnection);
